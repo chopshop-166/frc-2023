1c5347e10524b7259b9e1ee9d16e16f218ccdfe7
Timothy Bevis <tycheetah109@gmail.com>
2023-01-21 10:42:06 -0500

    Begin implementing PID and adding set-points

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -8,6 +8,8 @@ import java.util.function.DoubleSupplier;
 import com.chopshop166.chopshoplib.commands.SmartSubsystemBase;
 import com.chopshop166.chopshoplib.motors.SmartMotorController;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.maps.subsystems.ArmMap;
@@ -16,6 +18,7 @@ public class Arm extends SmartSubsystemBase {
 
     private ArmMap map;
     private SmartMotorController motor = new SmartMotorController();
+    PIDController pid = new PIDController(0, 0, 0);
 
     public Arm(ArmMap map) {
         this.map = map;
@@ -41,13 +44,11 @@ public class Arm extends SmartSubsystemBase {
             return length;
         }
 
-        public double getAgain() {
+        public double getAngle() {
             return angle;
         }
     }
 
-    // This stuff is important (assigned this part) \/
-    //
     public final double SPEED = 1;
 
     public CommandBase extend(DoubleSupplier motorSpeed) {
@@ -70,27 +71,17 @@ public class Arm extends SmartSubsystemBase {
         });
     }
 
-    public CommandBase retract(DoubleSupplier motorSpeed) {
-        return run(() -> {
-            motor.set(motorSpeed.getAsDouble());
-        });
+    public CommandBase movetoLow() {
+        return moveToDistance(Level.LOW.get(), 2);
     }
 
-    public CommandBase retractDistance(double distance, double speed) {
-        return cmd("Retract Distance").onInitialize(() -> {
-            motor.set(speed);
-        }).runsUntil(() -> {
-            if (distance == motor.getEncoder().getDistance()) {
-                motor.stopMotor();
-                return true;
-            } else {
-                return false;
-            }
-        });
+    public CommandBase movetoMedium() {
+        return moveToDistance(Level.MEDIUM.get(), 2);
     }
 
-    // End of important stuff
-    //
+    public CommandBase movetoHigh() {
+        return moveToDistance(Level.HIGH.get(), 2);
+    }
 
     public CommandBase rotate() {
         return runOnce(() -> {
@@ -112,5 +103,25 @@ public class Arm extends SmartSubsystemBase {
     public void periodic() {
         // This method will be called once per scheduler run
         // Use this for any background processing
+    }
+
+    // Will I need this? I don't think so but I'm not deleting it
+    public CommandBase retract(DoubleSupplier motorSpeed) {
+        return run(() -> {
+            motor.set(motorSpeed.getAsDouble());
+        });
+    }
+
+    public CommandBase retractDistance(double distance, double speed) {
+        return cmd("Retract Distance").onInitialize(() -> {
+            motor.set(speed);
+        }).runsUntil(() -> {
+            if (distance == motor.getEncoder().getDistance()) {
+                motor.stopMotor();
+                return true;
+            } else {
+                return false;
+            }
+        });
     }
 }
