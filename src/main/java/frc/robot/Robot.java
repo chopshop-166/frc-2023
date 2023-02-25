784954b39770aa81424bdb3c355a40890863b7c1
Timothy Bevis <tycheetah109@gmail.com>
2023-02-25 14:54:48 -0500

    Small change for integration

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -81,7 +81,7 @@ public class Robot extends CommandRobot {
         copilotController.rightBumper().whileTrue(armRotate.resetZero(() -> copilotController.getTriggers()));
         // will need buttons for the scoring positions
         copilotController.povUp()
-                .whileTrue(armRotate.moveTo(EnumLevel.HIGH_SCORE).andThen(arm.moveTo(EnumLevel.HIGH_SCORE)));
+                .whileTrue(armRotate.moveTo(EnumLevel.HIGH_SCORE));
         copilotController.povRight()
                 .whileTrue(armRotate.moveTo(EnumLevel.MEDIUM_SCORE).andThen(arm.moveTo(EnumLevel.MEDIUM_SCORE)));
         copilotController.y().whileTrue(armRotate.moveTo(EnumLevel.HPS_PICKUP));
