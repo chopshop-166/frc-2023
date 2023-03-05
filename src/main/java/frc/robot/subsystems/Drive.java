83c52c6daa17b0a3ea4a278d6cecd18b40f1d4a1
Timothy Bevis <tycheetah109@gmail.com>
2023-03-05 11:23:12 -0500

    Cleanup

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -11,7 +11,6 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
