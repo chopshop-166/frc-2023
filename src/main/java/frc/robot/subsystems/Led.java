b4ba60a73346bf8be86dbcdcd525bd0800fad27b
Timothy Bevis <tycheetah109@gmail.com>
2023-03-08 20:29:37 -0500

    Fix Fire Leds (?)

--- src/main/java/frc/robot/subsystems/Led.java
+++ src/main/java/frc/robot/subsystems/Led.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.maps.subsystems.LedMap;
-import frc.robot.util.FireLeds;
 
 public class Led extends SmartSubsystemBase {
 
@@ -95,10 +94,39 @@ public class Led extends SmartSubsystemBase {
         });
     }
 
+    private final int NUM_LEDS = ledBuffer.getLength();
+    public final byte[] heat = new byte[NUM_LEDS];
+
+    public void fire(int flameHeight, int sparks, int delayDuration) {
+        int cooldown;
+
+        // Cool down each cell a little
+        for (int i = 0; i < NUM_LEDS; i++) {
+            cooldown = (int) (Math.random() * ((flameHeight * 10) / NUM_LEDS + 2));
+
+            if (cooldown > heat[i]) {
+                heat[i] = 0;
+            } else {
+                heat[i] = (byte) (heat[i] - cooldown);
+            }
+        }
+
+        // Heat from each cell drifts up and diffuses slightly
+        for (int k = NUM_LEDS - 1; k >= 2; k--) {
+            heat[k] = (byte) ((heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3);
+        }
+
+        // Randomly ignite new sparks near bottom of the flame
+        if (Math.random() * 255 < sparks) {
+            int y = (int) (Math.random() * 7);
+            heat[y] = (byte) (heat[y] + (int) (Math.random() * (160 - 255 + 1) + 160));
+        }
+    }
+
     public CommandBase Fire() {
         return cmd("Make leds fire").onExecute(() -> {
-            for (int i = 0; i < FireLeds.heat.length; i++) {
-                Color color = new Color(FireLeds.heat[i] / 255.0, 0, 0);
+            for (int i = 0; i < heat.length; i++) {
+                Color color = new Color(heat[i] / 255.0, 0, 0);
                 ledBuffer.setLED(i, color);
             }
             led.setData(ledBuffer);
