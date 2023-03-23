61985e07ed9257e49b7db0090709dd8759a5f32d
Finn6360 <finn6360@duck.com>
2023-03-22 20:26:48 -0400

    Fixed position 4,5,6 drive out and into community location.

--- src/main/java/frc/robot/auto/ConeStation.java
+++ src/main/java/frc/robot/auto/ConeStation.java
@@ -7,12 +7,12 @@ public enum ConeStation {
             AutoPath.INTO_COMMUNITY_1_2_3),
     STATION_3(3, AutoPath.UP_TO_CONE_STATION_3, AutoPath.BACKED_UP_3, AutoPath.OUT_OF_COMMUNITY_1_2_3,
             AutoPath.INTO_COMMUNITY_1_2_3),
-    STATION_4(4, AutoPath.UP_TO_CONE_STATION_4, AutoPath.BACKED_UP_4, AutoPath.OUT_OF_COMMUNITY_1_2_3,
-            AutoPath.INTO_COMMUNITY_1_2_3),
-    STATION_5(5, AutoPath.UP_TO_CONE_STATION_5, AutoPath.BACKED_UP_5, AutoPath.OUT_OF_COMMUNITY_1_2_3,
-            AutoPath.INTO_COMMUNITY_1_2_3),
-    STATION_6(6, AutoPath.UP_TO_CONE_STATION_6, AutoPath.BACKED_UP_6, AutoPath.OUT_OF_COMMUNITY_1_2_3,
-            AutoPath.INTO_COMMUNITY_1_2_3);
+    STATION_4(4, AutoPath.UP_TO_CONE_STATION_4, AutoPath.BACKED_UP_4, AutoPath.OUT_OF_COMMUNITY_4_5_6,
+            AutoPath.INTO_COMMUNITY_4_5_6),
+    STATION_5(5, AutoPath.UP_TO_CONE_STATION_5, AutoPath.BACKED_UP_5, AutoPath.OUT_OF_COMMUNITY_4_5_6,
+            AutoPath.INTO_COMMUNITY_4_5_6),
+    STATION_6(6, AutoPath.UP_TO_CONE_STATION_6, AutoPath.BACKED_UP_6, AutoPath.OUT_OF_COMMUNITY_4_5_6,
+            AutoPath.INTO_COMMUNITY_4_5_6);
 
     public final int number;
     public final AutoPath upToStation;
