0d96e9bff6d8f6178a6432d6562ccefbd9ea526a
Helen Saunders <helen.elizabeth.saunders@gmail.com>
2023-03-24 17:44:36 -0400

    PR Changes

--- src/main/java/frc/robot/maps/FrostBiteMap.java
+++ src/main/java/frc/robot/maps/FrostBiteMap.java
@@ -26,7 +26,6 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.DutyCycle;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
