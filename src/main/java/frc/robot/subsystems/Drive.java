42ab717d9b4234f5148a26b197ae373f56252a8d
Timothy Bevis <tycheetah109@gmail.com>
2023-03-29 19:33:31 -0400

    Updates

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -332,7 +332,7 @@ public class Drive extends SmartSubsystemBase {
                 move(0.0, UNTIL_TIPPED_SPEED, 0.0);
             }
 
-        }).runsUntil(() -> Math.abs(this.getTilt()) > 13);
+        }).runsUntil(() -> Math.abs(this.getTilt()) > 10);
     }
 
     public double getTilt() {
