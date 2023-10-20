a80431e2e52761b0412c18a510f3218d826f3bfe
Liam.murray <liammurray2015@icloud.com>
2023-10-19 20:41:47 -0400

    Updated absolute encoder value

--- src/main/java/frc/robot/maps/FrostBiteMap.java
+++ src/main/java/frc/robot/maps/FrostBiteMap.java
@@ -177,7 +177,7 @@ public class FrostBiteMap extends RobotMap {
         absEncoder.setDutyCycleRange(1.0 / 1025.0, 1024.0 / 1025.0);
         absEncoder.setDistancePerRotation(-360);
         // Adjust this to move the encoder zero point to the retracted position
-        absEncoder.setPositionOffset(66.8);
+        absEncoder.setPositionOffset(54.8);
 
         CSFusedEncoder fusedEncoder = new CSFusedEncoder(encoder, absEncoder);
 
